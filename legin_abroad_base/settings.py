1d6a86277d5ca39b190d9c9b71f8c949daac53f2
AlexiLJ <leiko.oleksandr@gmail.com>
2024-01-27 13:45:47 +0100

    added installed apps to settings.py and updated requirements.txt accordingly, established models.py

--- legin_abroad_base/settings.py
+++ legin_abroad_base/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4'
+    'bootstrap4',
+    'imagekit',
+    'storages', #aws
+    'taggit',
+    'ckeditor_uploader',
     'legin_abroad'
 ]
 
