5b8ba7fa77fec1edd33720d01140b0592b7fa8e2
AlexiLJ <leiko.oleksandr@gmail.com>
2024-09-26 21:41:09 +0200

    settings.py

--- legin_abroad_base/settings.py
+++ legin_abroad_base/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from env_handler import var_getter
+from .env_handler import var_getter
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
