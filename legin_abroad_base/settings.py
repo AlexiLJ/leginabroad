c93ad19fcf0e86dfeb49f8aac2da1e61381dc4c8
AlexiLJ <leiko.oleksandr@gmail.com>
2024-09-27 15:18:01 +0200

    settings.py

--- legin_abroad_base/settings.py
+++ legin_abroad_base/settings.py
@@ -191,8 +191,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 # MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
