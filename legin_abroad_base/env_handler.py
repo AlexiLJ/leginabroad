8f000ea3fe0ebcc1e6b82f4fe4bd691d29029004
AlexiLJ <leiko.oleksandr@gmail.com>
2024-10-01 23:50:04 +0200

    settings.py

--- legin_abroad_base/env_handler.py
+++ legin_abroad_base/env_handler.py
@@ -10,12 +10,12 @@ def var_getter(name: str, storage: str|None = "/root/.django_env"):
     :param storage: str|None
     :return: Any
     '''
-    if storage:
-        with open(pathlib.Path(storage)) as st:
-            var = json.load(st)
-            var = var.get(name)
-    else:
-        var = os.environ.get(name)
+    # if storage:
+    with open(pathlib.Path(storage)) as st:
+        var = json.load(st)
+        var = var.get(name)
+    # else:
+    #     var = os.environ.get(name)
     print(name, var)
     return var
 
