c4449ec8f0ea9c400a99453a3386b80835aa4953
AlexiLJ <leiko.oleksandr@gmail.com>
2024-11-04 23:31:22 +0100

    notes and comments *

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 sudo systemctl restart gunicorn
 sudo systemctl daemon-reload  # reload the systemd manager configuration
 sudo systemctl restart gunicorn.socket gunicorn.service
-nginx -t && sudo systemctl restart nginx
+sudo nginx -t && sudo systemctl restart nginx
