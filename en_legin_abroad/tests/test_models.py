5f39b3d96f5f57863a3d6ee30607be2b5f9bc083
AlexiLJ <leiko.oleksandr@gmail.com>
2025-01-03 17:19:01 +0100

    restarti script changes

--- en_legin_abroad/tests/test_models.py
+++ en_legin_abroad/tests/test_models.py
@@ -22,6 +22,10 @@ class EnArticleEnSectionTest(TestCase):
         self.assertTrue(self.first_sections.date_added == self.first_article.date_added)
         self.assertIn("TEST", str(self.first_article.body))
 
+    def test_absolute_sections_url(self):
+        self.assertIn(f"{self.first_sections.sslug}",
+                      self.first_sections.get_absolute_url())
+
     def test_absolute_article_url(self):
         self.assertIn(f"{self.first_sections.sslug}/{self.first_article.slug}",
                       self.first_article.get_absolute_url())
--- restart.sh
+++ restart.sh
@@ -1,3 +1,41 @@
+#!/bin/bash
+
+# Initialize the variable for the -t flag
+run_tests=false
+venv_path="venv"
+
+# Parse the arguments
+while getopts "td:" opt; do
+  case $opt in
+    t)
+      run_tests=true
+      ;;
+    d)
+      venv_path=$OPTARG
+      ;;
+    *)
+      echo "Usage: $0 [-t] [-d <venv_path>]"
+      exit 1
+      ;;
+  esac
+done
+
+# If the -t flag is passed, run tests
+if [ "$run_tests" = true ]; then
+  # activate venv
+  if [ -d "$venv_path" ]; then
+    # shellcheck disable=SC1090
+    source "$(pwd)/$venv_path/bin/activate"
+    echo "Virtual environment activated: $venv_path"
+  else
+    echo "Error: Virtual environment not found at $venv_path"
+    exit 1
+  fi
+  echo "Running tests..."
+  python3 manage.py test || { echo "Tests failed! Aborting."; deactivate; exit 1; }
+fi
+
+echo "Reloading services."
 sudo systemctl restart gunicorn
 sudo systemctl daemon-reload  # reload the systemd manager configuration
 sudo systemctl restart gunicorn.socket gunicorn.service
