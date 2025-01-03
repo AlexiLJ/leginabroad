050bc60f448bd600758b1a459eafeb8dc1c6ea1e
AlexiLJ <leiko.oleksandr@gmail.com>
2025-01-03 17:46:12 +0100

    added collectstatic

--- restart.sh
+++ restart.sh
@@ -2,10 +2,22 @@
 
 # Initialize the variable for the -t flag
 run_tests=false
+# shellcheck disable=SC2034
+run_collectstatic=false
 venv_path="venv"
 
+activate_venv() {
+  if [ -d "$1" ]; then
+    sudo source "$(pwd)/$1/bin/activate"
+    echo "Virtual environment activated: $1"
+  else
+    echo "Error: Virtual environment not found at $1"
+    exit 1
+  fi
+}
+
 # Parse the arguments
-while getopts "td:" opt; do
+while getopts "tdc:" opt; do
   case $opt in
     t)
       run_tests=true
@@ -13,8 +25,11 @@ while getopts "td:" opt; do
     d)
       venv_path=$OPTARG
       ;;
+    c)
+      run_collectstatic=true
+      ;;
     *)
-      echo "Usage: $0 [-t] [-d <venv_path>]"
+      echo "Usage: $0 [-t] [-c] [-d <venv_path>]"
       exit 1
       ;;
   esac
@@ -23,16 +38,17 @@ done
 # If the -t flag is passed, run tests
 if [ "$run_tests" = true ]; then
   # activate venv
-  if [ -d "$venv_path" ]; then
-    # shellcheck disable=SC1090
-    source "$(pwd)/$venv_path/bin/activate"
-    echo "Virtual environment activated: $venv_path"
-  else
-    echo "Error: Virtual environment not found at $venv_path"
-    exit 1
-  fi
+  activate_venv "$venv_path"
   echo "Running tests..."
-  python3 manage.py test || { echo "Tests failed! Aborting."; deactivate; exit 1; }
+  sudo python3 manage.py test || { echo "Tests failed! Aborting."; deactivate; exit 1; }
+  deactivate
+fi
+
+if [ "$run_collectstatic" = true ]; then
+  activate_venv "$venv_path"
+  echo "Running python3 manage.py collectstatic"
+  sudo python3 manage.py collectstatic || { echo "Tests failed! Aborting."; deactivate; exit 1; }
+  deactivate
 fi
 
 echo "Reloading services."
