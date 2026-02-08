e7eb2c5182055a135838daa879fbf81f126b7f9e
AlexiLJ <leiko.oleksandr@gmail.com>
2026-02-08 12:40:51 +0100

    urls

--- legin_abroad_base/urls.py
+++ legin_abroad_base/urls.py
@@ -28,10 +28,10 @@ from django.urls import include, path
 #     'article': ArticleSitemap,
 #     'section': SectionSitemap,
 # }
+
 urlpatterns = [
     path("admin_LA/", admin.site.urls),
     path("", include("en_legin_abroad.urls")),
-    # path('', include('legin_abroad.urls')),
     # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path("ckeditor/", include("ckeditor_uploader.urls")),
 ]
